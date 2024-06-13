aa71a81d3425bbed727456173ce944b1639df5fe
Neha <109570765+mahawarneha@users.noreply.github.com>
2024-06-13 15:44:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+  let string = " ";
+    let buttons = document.querySelectorAll('.button_numbers');
+
+    Array.from(buttons).forEach((button) => {
+        button.addEventListener('click', (e) => {
+
+            if (e.target.innerHTML == '=') {
+                string = eval(string);
+                document.querySelector('input').value = string;
+            }
+            else if (e.target.innerHTML == 'C') {
+                string = "";
+                document.querySelector('input').value = string;
+            }
+            else {
+                console.log(e.target)
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        })
+    })
